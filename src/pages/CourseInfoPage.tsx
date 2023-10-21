8ea0baabaf793f69abefe760fa5b80a273b07100
itsarasib <itsara.pliansart@gmail.com>
2023-10-22 00:22:36 +0700

    change bg

--- src/pages/CourseInfoPage.tsx
+++ src/pages/CourseInfoPage.tsx
@@ -16,11 +16,7 @@ const CourseInfoPage: React.FC = () => {
   const course = mockData[Number(id) - 1];
   const mySyllabus = course.syllabus.map((syllabus) => {
     return (
-<<<<<<< HEAD
-      <Box border="1px solid black" p="20px" borderRadius="16px" bgColor='green'>
-=======
-      <Box border="1px solid black" p="20px" borderRadius="16px" bgColor="red">
->>>>>>> 68542e0274fa96af37f941297c2e05d0dac18def
+      <Box border="1px solid black" p="20px" borderRadius="16px" bgColor='red'>
         <p> name: {syllabus.name}</p>
         <p> hour: {syllabus.hour} </p>
         <p> description: {syllabus.description} </p>
